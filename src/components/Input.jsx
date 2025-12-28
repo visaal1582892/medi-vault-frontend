9c838eb9e6c08fa86301833c802fc634e7919167
Rohit Varma Datla <visaal1582892@gmail.com>
2025-12-28 11:21:53 +0530

    completed login ui

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,16 @@
 import { EyeOff, Eye } from 'lucide-react';
 import { useRef, useState } from 'react';
 
-const Input = ({ type, label }) => {
+const Input = ({ type, label, style }) => {
 
     const inputRef = useRef(null);
     const [showPassword, setShowPassword] = useState(false);
 
-    const handleInputClick = (event) => {
-        if (document.activeElement !== inputRef.current) {
-            inputRef.current.focus();
-        }
-    }
-
     return (
-        <div className='flex w-[80%] border border-slate-500 relative group rounded-sm hover:bg-slate-200 focus-within:border-2 h-13 group cursor-text pb-1' onClick={handleInputClick}>
-            <input type={type} ref={inputRef} className='w-full h-[60%] outline-none p-2 mt-auto rounded-sm peer text-sm' />
-            <p className='absolute left-3 text-sm tracking-wider font-medium top-1/2 -translate-y-1/2 peer-focus-within:top-1 peer-focus-within:text-[10px] peer-focus-within:translate-y-0 peer-focus-within:font-normal transition-all duration-150 pointer-events-none peer-focus-within:text-blue-700'>{label}</p>
-            <button
+        <div className={`flex w-full border border-slate-500 relative group rounded-sm hover:bg-slate-100 focus-within:border-2 h-12 group cursor-text pb-1 ${style}`}>
+            <input placeholder=' ' type={showPassword==true?"text":type} ref={inputRef} className='w-full h-full outline-none px-2 pt-7 pb-2 mt-auto rounded-sm peer text-sm' />
+            <p className='input-label'>{label}</p>
+            {label=="Password"?<button
                 type="button"
                 onClick={(event) => {
                     setShowPassword(prev => !prev);
@@ -24,8 +18,8 @@ const Input = ({ type, label }) => {
                 }}
                 className="absolute right-3 top-1/2 -translate-y-1/2 text-slate-500"
             >
-                {showPassword ? <EyeOff /> : <Eye />}
-            </button>
+                {showPassword ? <EyeOff size={17} /> : <Eye size={17} />}
+            </button>:null}
         </div>
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,37 @@
 import Input from "./Input"
+import { Link } from "react-router-dom"
 
 const Login = () => {
 
     return (
-        <div className='flex flex-col justify-start items-center-safe h-[70vh] border-2 border-slate-500 w-[35%] rounded-lg max-w-[90%] hovering-effect bg-slate-100'>
-            <p className='font-medium animate-pulse m-8'>Login to Access Your <span className='font-bold'>MEDI VAULT</span></p>
-            <Input type="text" label="Email" />
-            <Input type="password" label="Password" />
+
+        <div className='flex flex-col justify-start items-center-safe h-[70vh] w-[35%] rounded-lg min-w-90 hovering-effect bg-slate-200'>
+            <p className='font-medium animate-pulse m-5'>Login to Access Your <span className='font-bold'>MEDI VAULT</span></p>
+
+            {/* Login with credentials */}
+            <div className="w-[70%]">
+                <Input type="email" label="Email" style="mb-4" />
+                <Input type="password" label="Password" style={"pr-10 mb-4"} />
+                <button className="bg-slate-900 text-slate-100 w-full p-1.5 text-sm rounded-md hover:bg-slate-600 cursor-pointer mb-7">Login</button>
+            </div>
+
+            {/* Other Options Label */}
+            <label className="flex justify-center-safe items-center-safe w-[70%] mb-7">
+                <label className="border w-full"></label>
+                <p className="text-xs text-slate-600 w-full whitespace-nowrap m-2">Other log in options</p>
+                <label className="border w-full"></label>
+            </label>
+
+            {/* Login with Other Options */}
+            <div className="flex w-[70%] justify-center-safe items-center-safe mb-7">
+
+                {/* Google Login */}
+                <button className="border w-10 cursor-pointer rounded-sm hover:bg-slate-100">
+                    <img src="/images/googleIcon.png" alt="googleIcon" className="w-full p-2" />
+                </button>
+            </div>
+
+            <p className="text-xs">Don't have an account? <Link to="">Register</Link></p>
         </div>
     )
 }
